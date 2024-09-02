5e675edae3bf65d2705d8da6cb545a725bc53b10
Nicolly Rosso <110644347+NicollyRossoCyber@users.noreply.github.com>
2024-09-02 13:49:29 -0300

    Update login.php
    login

--- app/view/login/login.php
+++ app/view/login/login.php
@@ -1,66 +1,41 @@
 <?php
-#Nome do arquivo: login/login.php
-#Objetivo: interface para logar no sistema
+# Nome do arquivo: login.php
+# Objetivo: interface para logar no sistema
 
 require_once(__DIR__ . "/../include/header.php");
 ?>
 
-<link rel="stylesheet" href="<?= BASEURL ?>/view/css/login.css">
+<div class="container form-container d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+    <div class="form-content text-center">
+        <!-- Título "IFSHARE" centralizado -->
+        <h1 class="font-abril title-ifshare mb-4">IFSHARE</h1>
 
-<div class="container">
-    <div class="row" style="margin-top: 20px;">
-        <div class="col-12 coluna1">
-            <h4>Fazer Login</h4>
-            <br>
+        <!-- Texto do formulário centralizado -->
+        <h4 class="mb-4">Informe os dados para logar:</h4>
 
-            <!-- Formulário de login -->
-            <form id="frmLogin" action="./LoginController.php?action=logon" method="POST">
-                <div class="form-group">
-                    <label for="txtEmail">
-                        <?php
-                            if(isset($arrayMsg['emailLogin'])){
-                                echo("<div class='alert alert-danger'>" . $arrayMsg['emailLogin'] . "</div>");
-                            }else{
-                                echo "Email:";
-                            }
-                        ?></label>
-                    <input type="text" class="form-control" name="email" id="txtEmail"
-                        placeholder="Informe o email"
-                        value="<?php echo isset($dados['email']) ? $dados['email'] : '' ?>" />
-                </div>
-
-                <div class="form-group">
-                    <label for="txtSenha"><?php
-                            if(isset($arrayMsg['senhaLogin'])){
-                                echo("<div class='alert alert-danger'>" . $arrayMsg['senhaLogin'] . "</div>");
-                            }else{
-                                echo "Email:";
-                            }
-                        ?></label>
-                    <input type="password" class="form-control" name="senha" id="txtSenha"
-                        placeholder="Informe a senha"
-                        value="<?php echo isset($dados['senha']) ? $dados['senha'] : '' ?>" />
-                </div>
-
-                <button type="submit" class="btn btn-custom">Fazer login</button>
-
-                <a href="/PFC/app/controller/UsuarioController.php?action=createCadastro" 
-                   class="btn btn-custom">Cadastrar-se</a>
-                
-            </form>
-            
-        </div>
-
-        <div class="row">
-
-            <div class="col-12 coluna2">
-                <?php include_once(__DIR__ . "/../include/msg.php") ?>
+        <!-- Formulário de Login -->
+        <form id="frmLogin" action="./LoginController.php?action=logon" method="POST" class="text-start">
+            <div class="mb-3">
+                <label for="txtEmail" class="form-label">Email:</label>
+                <input type="text" class="form-control" name="email" id="txtEmail"
+                       placeholder="Informe o email"
+                       value="<?php echo isset($dados['email']) ? $dados['email'] : '' ?>" />        
             </div>
 
-        </div>
+            <div class="mb-3">
+                <label for="txtSenha" class="form-label">Senha:</label>
+                <input type="password" class="form-control" name="senha" id="txtSenha"
+                       placeholder="Informe a senha"
+                       value="<?php echo isset($dados['senha']) ? $dados['senha'] : '' ?>" />        
+            </div>
+
+            <button type="submit" class="btn btn-custom w-100">Logar</button>
+        </form>
+        <?php include_once(__DIR__ . "/../include/msg.php") ?>
     </div>
 </div>
 
 <?php
+# Incluindo o footer
 require_once(__DIR__ . "/../include/footer.php");
 ?>
