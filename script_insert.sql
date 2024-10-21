82d9edd693680f652982d1b4495e8c419af63f10
Matheus Cardoso <101215639+MathuCardoso@users.noreply.github.com>
2024-10-21 15:20:35 -0300

    Delete script_insert.sql

--- script_insert.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO usuario (nomeCompleto, nomeUsuario, email, senha,
-            bio, tipoUsuario, dataCriacao, compMatricula)
-            
-            VALUES ("Matheusinho", "MathUzinho", "math792@gmail.com", 12345, NULL, 
-            "ESTUDANTE", CURRENT_DATE, NULL)
