bdaf006f026a14f7256f43e739599c4bbecd4af5
Matheus Cardoso <101215639+MathuCardoso@users.noreply.github.com>
2024-10-25 17:25:35 -0300

    Add files via upload

---
+++ controller/CurtidaController.php
@@ -0,0 +1,78 @@
+<?php
+require_once(__DIR__ . "/../dao/PostagemDao.php");
+require_once(__DIR__ . "/../dao/UsuarioDAO.php");
+require_once(__DIR__ . "/Controller.php");
+require_once(__DIR__ . "/../model/Curtida.php");
+require_once(__DIR__ . "/../dao/CurtidaDAO.php");
+
+class CurtidaController extends Controller
+{
+    private CurtidaDAO $curtidaDao;
+
+    public function __construct()
+    {
+        if (!$this->usuarioLogado())
+            exit;
+
+        $this->curtidaDao = new CurtidaDAO();
+
+        $this->handleAction();
+    }
+
+    public function isLiked(): bool
+    {
+        $idPostagem = $_GET['id'] ?? $_GET['id'] ?? null;
+
+        if (!$idPostagem) {
+            return false;
+        }
+
+        $idUsuario = $_SESSION['usuarioLogadoId'] ?? null;
+
+        if (!$idUsuario) {
+            return false;
+        }
+
+        $likeExistente = $this->curtidaDao->isLiked($idPostagem, $idUsuario);
+
+        return (bool) $likeExistente;
+    }
+
+
+    public function likeDislike()
+    {
+        // Captura o id da postagem via GET ou POST
+        $idPostagem = $_POST['id'] ?? $_GET['id'] ?? null;
+
+        if (!$idPostagem) {
+            echo "ID da postagem não fornecido!";
+            exit;
+        }
+        $idUsuario = $_SESSION['usuarioLogadoId']; // Ou use o método que obtém o usuário logado
+
+        // Verificar se já existe uma curtida para esta postagem e usuário
+        $likeExistente = $this->curtidaDao->isLiked($idPostagem, $idUsuario);
+
+        if ($likeExistente) {
+            // Se já existe, remover a curtida
+            $this->curtidaDao->delLike($likeExistente['id']);
+            echo "Curtida removida (deslike)";
+        } else {
+            // Se não existe, inserir a curtida
+            $curtida = new Curtida();
+            $post = new Post();
+            $usuario = new Usuario();
+            $post->setId($idPostagem);
+
+            $curtida->setPost($post);
+            $curtida->setUsuario($usuario);
+
+            $this->curtidaDao->insertLike($curtida);
+            echo "Curtida registrada (like)";
+        }
+
+        header("location: " . "/PFC/app/controller/PostagemController.php?action=viewPost&id=" . $idPostagem);
+    }
+}
+
+new CurtidaController();
---
+++ dao/PostagemDao.php
@@ -0,0 +1,134 @@
+<?php
+
+#INCLUDES
+
+include_once(__DIR__ . "/../connection/Connection.php");
+include_once(__DIR__ . "/../model/Post.php");
+include_once(__DIR__ . "/../model/Usuario.php");
+include_once(__DIR__ . "/../service/LoginService.php");
+
+class PostagemDAO
+{
+    private LoginService $loginService;
+
+    public function __construct()
+    {
+        $this->loginService = new LoginService();        
+    }
+
+
+    #LISTAGEM DE POSTAGENS
+
+    public function listPosts()
+    {
+
+        $conn = Connection::getConnection();
+
+        $sql = "SELECT *FROM postagem ORDER BY id DESC";
+        $stm = $conn->prepare($sql);
+
+        $stm->execute();
+        $result = $stm->fetchAll();
+
+        return $this->mapPostagens($result);
+    }
+
+    ####################################################################################
+
+    #INSERÇÃO DE POSTAGEM
+
+    public function insertPost(Post $post)
+    {
+        $conn = Connection::getConnection();
+
+        $sql = "INSERT INTO postagem (imagem, legenda, dataPostagem, idUsuario)" .
+            " VALUES (:imagem, :legenda, now(), :idUsuario)";
+
+        $stm = $conn->prepare($sql);
+        $stm->bindValue("imagem", $post->getImagem());
+        $stm->bindValue("legenda", $post->getLegenda());
+        $stm->bindValue("idUsuario", $this->loginService->getIdUsuarioLogado());
+
+        $stm->execute();
+    }
+
+    ####################################################################################
+
+    #DELETA UMA POSTAGEM PELO ID
+
+    public function deletePostById(int $id)
+    {
+        $conn = Connection::getConnection();
+
+        $sql = "DELETE FROM postagem WHERE id = :id";
+
+        $stm = $conn->prepare($sql);
+        $stm->bindValue("id", $id);
+        $stm->execute();
+    }
+
+    ####################################################################################
+
+    #CONTA O NÚMERO DE POSTAGENS DO SISTEMA
+
+    public function count() {
+        $conn = Connection::getConnection();
+
+        $sql = "SELECT COUNT(*) total FROM postagem";
+        $stm = $conn->prepare($sql);
+        $stm->execute();
+        $result = $stm->fetchAll();
+
+        return $result[0]["total"];
+    }
+
+    ####################################################################################
+
+    #BUSCA UMA POSTAGEM PELO ID
+
+    public function findById(int $id)
+    {
+        $conn = Connection::getConnection();
+
+        $sql = "SELECT * FROM postagem p WHERE p.id = ?";
+        $stm = $conn->prepare($sql);
+
+        $stm->execute([$id]);
+        $result = $stm->fetchAll();
+
+        $postagens = $this->mapPostagens($result);
+
+        if (count($postagens) == 1)
+            return $postagens[0];
+        elseif (count($postagens) == 0)
+            return null;
+
+        die("UsuarioDAO.findById()" .
+            " - Erro: mais de uma postagem encontrada.");
+    }
+
+    ####################################################################################
+
+    #CONVERTE REGISTRO DA BASE EM OBJETO
+
+    private function mapPostagens($result)
+    {
+        $postagens = array();
+        foreach ($result as $reg) {
+            $post = new Post();
+            $post->setId($reg['id']);
+            $post->setImagem($reg['imagem']);
+            $post->setLegenda($reg['legenda']);
+            $post->setDataPostagem($reg['dataPostagem']);
+
+            $usuario = new Usuario();
+            $usuario->setId($reg['idUsuario']);
+            $post->setUsuario($usuario);
+
+
+            array_push($postagens, $post);
+        }
+
+        return $postagens;
+    }
+}
